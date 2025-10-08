fc29af96562e09b2a91545a01d466ee91fc56e4d
Vladkata-01 <vladimirvvasilev111@gmail.com>
2025-10-08 19:56:22 +0300

    Update cheapest_transport.py

--- cheapest_transport.py
+++ cheapest_transport.py
@@ -1,16 +1,16 @@
-kilometers = int(input("Amount of kilometers travelled: "))
-time = input("Day or night: ")
+kilometers = int(input("Amount of kilometers travelled:"))
+time = input("Day or night:")
 sum = 0
 
 if kilometers > 100:
-    sum = 0.06 * kilometers
+    sum = 0.06 *kilometers
 elif kilometers > 20:
-    sum = 0.09 * kilometers
+    sum = 0.09 *kilometers
 else:
     if time == "day":
-        sum = 0.70 + 0.79 * kilometers
+        sum = 0.70 + 0.79 *kilometers
     elif time == "night":
-        sum = 0.70 + 0.90 * kilometers
+        sum = 0.70 + 0.90 *kilometers
 
 print(f"sum = {sum}")
 

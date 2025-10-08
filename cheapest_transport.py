2305e9a5e2f9bc9167083a60e0991928e7bdfc88
Vladkata-01 <vladimirvvasilev111@gmail.com>
2025-10-08 19:52:06 +0300

    Create cheapest_transport.py

---
+++ cheapest_transport.py
@@ -0,0 +1,16 @@
+kilometers = int(input("Amount of kilometers travelled: "))
+time = input("Day or night: ")
+sum = 0
+
+if kilometers > 100:
+    sum = 0.06 * kilometers
+elif kilometers > 20:
+    sum = 0.09 * kilometers
+else:
+    if time == "day":
+        sum = 0.70 + 0.79 * kilometers
+    elif time == "night":
+        sum = 0.70 + 0.90 * kilometers
+
+print(f"sum = {sum}")
+
